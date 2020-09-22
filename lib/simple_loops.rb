484a9a8a57c12b2d532a3412da3870a3dd1611d2
Matthew Sallin <msallin@gmail.com>
2020-09-22 03:44:40 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -6,3 +6,11 @@ def loop_message_five_times(string)
       counter += 1
   end
 end
+
+def loop_message_n_times(string, int)
+  counter = 0
+  while counter < int
+      puts string
+      counter += 1
+  end
+end
